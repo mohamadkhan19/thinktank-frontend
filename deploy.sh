5cfa513957a66deefd97743c2caca42bad7ae611
praveen-elagudri <elagudripraveen@gmail.com>
2017-05-12 17:27:24 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 -#!/bin/sh
 ssh ubuntu@ip-172-31-15-68 <<EOF
 cd /home/ubuntu/praveen/thinktank 
-sudo kill -9 grunt
+ps -ef | grep grunt | grep -v grep | awk '{print $2}' | xargs kill
 sudo git pull       
 sudo npm restart&
  exit       
